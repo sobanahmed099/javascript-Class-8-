2c6c1c776c45c663a1e15e5a5875899d85c2be45
sobanahmed099 <sobanahmedkigs125@gmail.com>
2025-12-19 15:55:15 +0500

    Add files via upload

---
+++ Javascript class 8 Assignment 19/app.js
@@ -0,0 +1,63 @@
+// var a = prompt("Enter a positive integer")
+// console.log("Number " + a)
+// var b = Math.round(a)
+// console.log("Round Off Value " +b)
+// var c = Math.floor(b)
+// console.log("Floor Value " +c)
+// var d = Math.ceil(c)
+// console.log("Ceil Value " +d)
+
+
+// 
+// var e = prompt("Enter negative floating number")
+// console.log("Number " + e)
+// var f = Math.round(e)
+// console.log("Round Off Value " +f)
+// var g = Math.floor(f)
+// console.log("Floor Value " +g)
+// var h = Math.ceil(g)
+// console.log("Ceil Value " +h)
+
+
+
+// var i = -4
+// var j = Math.abs(i)
+// console.log("Absolut value of " + i +  " is " + j )
+
+
+// Dice
+// var k = Math.random()
+// console.log(k)
+// var kk = (k*6)+ 1
+// console.log(kk);
+// var kkk = Math.floor(kk)
+// console.log(kkk);
+
+
+// Coin Toss
+// var k = Math.random()
+// console.log(k)
+// var kk = (k*2)+ 1
+// console.log(kk);
+// var kkk = Math.floor(kk)
+// console.log(kkk);
+
+
+// var l = Math.random()
+// var ll = l*100 
+// var lll = Math.floor(ll)
+// console.log("Random Number between 1 to 100 is " + lll)
+
+
+// var m = prompt(" Enter your Weight in KG")
+// console.log("The Weight Of user is " + m+"KG")
+
+
+// var n = prompt("Enter  a number between 1 and 10.")
+// var nn = 6
+// if(n == nn){
+//     alert("Congratulations")
+// }
+
+
+
